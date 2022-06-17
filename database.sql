a33ed1d67e96321428d1564cc45cc139535fa5f5
Courtney Azar <courtney.a.azar@gmail.com>
2022-06-17 11:58:07 -0500

    updated db

--- database.sql
+++ database.sql
@@ -7,13 +7,13 @@ CREATE TABLE "pizza" (
 );
 
 INSERT INTO "pizza" ("name", "description", "price", "image_path")
-VALUES ('Tomato Soup','If you like pizza, but you hate the toppings, the cheese, and the crust, you''ll love this!',12.99,'images/pizza_photo.png'),
-('Onomatopizza','We start with a WHOMP of dough, SPLAT some marinara on it, PLOP enough cheese on there to make a mouse PEEP. Top it off with some SIZZLING bacon, and BOOM there it is! We guarantee you''ll SMACK your lips.',14.99,'images/pizza_photo.png'),
-('Pepperoni','Classic pizza with cheese and pepperoni. Baked with a traditional crust in our brick oven.',14.99,'images/pizza_photo.png'),
-('Over the Rainbow','Taste the rainbow! One ingredient of each color: pepperoni, doritos, pineapple, olives, cheese, peppers and onion. Complimentary water served in a spray bottle to taste an actual rainbow.',19.99,'images/pizza_photo.png'),
-('Chinese Firedragon','Pepperoni, pineapple and banana peppers.',15.99,'images/pizza_photo.png'),
-('Bad Date','Garlic, Onion and Pepperoni.',24.99,'images/pizza_photo.png'),
-('Another Little Pizza My Heart', 'Cheese Pizza. Personal size only.', 5.99,'images/pizza_photo.png');
+VALUES ('Tomato Soup','If you like pizza, but you hate the toppings, the cheese, and the crust, you''ll love this!',12.99,'images/Tomato-Soup.jpeg'),
+('Onomatopizza','We start with a WHOMP of dough, SPLAT some marinara on it, PLOP enough cheese on there to make a mouse PEEP. Top it off with some SIZZLING bacon, and BOOM there it is! We guarantee you''ll SMACK your lips.',14.99,'images/Bacon-pizza.jpeg'),
+('Pepperoni','Classic pizza with cheese and pepperoni. Baked with a traditional crust in our brick oven.',14.99,'images/Brick-oven-pepperoni.jpeg'),
+('Over the Rainbow','Taste the rainbow! One ingredient of each color: pepperoni, doritos, pineapple, olives, cheese, peppers and onion. Complimentary water served in a spray bottle to taste an actual rainbow.',19.99,'images/Rainbow-pizza.jpeg'),
+('Chinese Firedragon','Pepperoni, pineapple and banana peppers.',15.99,'images/Pepperoni-pineapple-pepper.jpeg'),
+('Bad Date','Garlic, Onion and Pepperoni.',24.99,'images/bad-date-pizza.jpeg'),
+('Another Little Pizza My Heart', 'Cheese Pizza. Personal size only.', 5.99,'images/Heart-Shaped-Pizza.jpeg');
 
 CREATE TABLE "orders" (
 	"id" SERIAL PRIMARY KEY,
